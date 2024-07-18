09aca49d7e379652e06409fbfc48d36f7124c524
rohxoxo <rohitnicbc@gmail.com>
2024-07-18 12:25:20 -0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <div className="w-full h-screen bg-zinc-900">
+      
+    </div>
+    </>
+  )
+}
+
+export default App
